aa8644134f4d89e15990f45a8ff71b6695f72d5d
Chen Zhengwei <chenzhengwei@inspur.com>
2021-06-07 10:03:59 +0800

    替换为applyPath方法

--- cmd/webhook-server/main.go
+++ cmd/webhook-server/main.go
@@ -143,7 +143,7 @@ func main() {
 	keyPath := filepath.Join(tlsDir, tlsKeyFile)
 
 	mux := http.NewServeMux()
-	mux.Handle("/mutate", admitFuncHandler(applySecurityDefaults))
+	mux.Handle("/mutate", admitFuncHandler(applyPath))
 	server := &http.Server{
 		// We listen on port 8443 such that we do not need root privileges or extra capabilities for this server.
 		// The Service object will take care of mapping this port to the HTTPS port 443.
